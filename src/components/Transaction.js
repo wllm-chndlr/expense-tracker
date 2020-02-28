afd53d0e70b53bdcdd5f584493f3e281033453af
William Chandler <chndlr.wllm@gmail.com>
2020-02-27 21:00:22 -0600

    additional UI components, initial global context

---
+++ src/components/Transaction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Transaction = ({ transaction }) => {
+    return (
+        <li className="minus">
+            {transaction.text}
+            <span>-$400</span>
+            <button className="delete-btn">
+                x
+            </button>
+        </li>
+    );
+};
