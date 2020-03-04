533f41a8895476e5429704dabff1ac3e348ad01c
William Chandler <chndlr.wllm@gmail.com>
2020-03-03 21:31:49 -0600

    dynamic balance, income/expense, transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState'
 
 export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
     return (
-        <li className="minus">
+        <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
             {transaction.text}
-            <span>-$400</span>
-            <button className="delete-btn">
+            <span>
+                {sign}${Math.abs(transaction.amount)}
+            </span>
+            <button
+                className="delete-btn"
+                onClick={() => deleteTransaction(transaction.id)}
+            >
                 x
             </button>
         </li>
