968dba30c2e33a74da03e588987b76231aab914f
William Chandler <chndlr.wllm@gmail.com>
2020-03-30 20:48:42 -0500

    Updates

---
+++ precache-manifest.86f684d7cf54d3ceb33b97fa5d19175c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec049047d3fbdcd14b7061f1cf86f092",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "2bdb9e64ab4bce10e6d2",
+    "url": "/expense-tracker/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "6c4a503a07e7c39835e6",
+    "url": "/expense-tracker/static/js/2.fe0fce3d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/expense-tracker/static/js/2.fe0fce3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bdb9e64ab4bce10e6d2",
+    "url": "/expense-tracker/static/js/main.10d42383.chunk.js"
+  },
+  {
+    "revision": "59a71736c99d25fe4e33",
+    "url": "/expense-tracker/static/js/runtime-main.e1909f45.js"
+  }
+]);
